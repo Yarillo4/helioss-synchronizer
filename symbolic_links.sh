c403eb360bb776146b46b0ff635ecd69369db3a6
yarillo4 <yarillo4@gmail.com>
2020-02-18 20:51:30 +0100

    Symbolic links

---
+++ symbolic_links.sh
@@ -0,0 +1,12 @@
+mkdir servers
+ln -s "/srv/daemon-data/0c4af646-1daf-4af1-884b-4c24c9813b8a" "./servers/PO3 Overworld [TEMP]"
+ln -s "/srv/daemon-data/31750782-112a-49a1-b79f-58a9ccb554c2" "./servers/Vanilla"
+ln -s "/srv/daemon-data/49254802-7458-4141-8c66-e40b664d06c2" "./servers/FAKESERVERFORVOTESITES"
+ln -s "/srv/daemon-data/67402a90-d461-498c-8b85-b0220d8a03e8" "./servers/All the Mods 3 Remix"
+ln -s "/srv/daemon-data/6867c336-2480-475e-bcbd-3511fcc7e32b" "./servers/Enigmatica 2 Expert [SKYBLOCK]"
+ln -s "/srv/daemon-data/6b04c789-56db-435d-b90f-73e66c9bcdea" "./servers/PO3 Skyblock"
+ln -s "/srv/daemon-data/77501e89-3338-49bb-8b14-291de957e9ff" "./servers/YAMPA"
+ln -s "/srv/daemon-data/8391cb39-3bca-42c2-baea-4982d8849679" "./servers/Omnifactory"
+ln -s "/srv/daemon-data/9f8ecb27-365e-459f-9aa4-55caee5ccdad" "./servers/MC Eternal"
+ln -s "/srv/daemon-data/a0174841-a39b-4540-82b3-7cdaca396707" "./servers/Bungee Integration"
+ln -s "/srv/daemon-data/ee19241b-746d-4a69-b452-d700a83ae98f" "./servers/Sponge Dev"
